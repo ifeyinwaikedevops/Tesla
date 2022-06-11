7413870561c2e5b15b12ca755f164832940a7ab7
IfeyinwaIke <105609604+IfeyinwaIke@users.noreply.github.com>
2022-06-11 21:03:18 +0100

    car servicing feature added

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ print ('Grace')
 print ('Ivo')
 print ('Chidi')
 print ('Glory')
-#Jesus is Lord
 #Jesus is alive
 #bug removed
+print ("car services automated")
+print ("hello $user your car needs servicing")
